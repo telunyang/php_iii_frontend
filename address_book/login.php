acd59bca95e2e1cfcda3cfaef369557d3f476c03
DarrenYang <telunyang@gmail.com>
2021-04-23 00:34:04 +0800

    更新: 修改登入錯誤時，應道向 index.php 而非當前 login.php 的問題

--- address_book/login.php
+++ address_book/login.php
@@ -9,10 +9,10 @@ require_once('./db.inc.php');
 
 if( isset($_POST['username']) && isset($_POST['pwd']) ){
     //SQL 語法
-    $sql = "SELECT `username`, `pwd` ";
-    $sql.= "FROM `admin` ";
-    $sql.= "WHERE `username` = ? ";
-    $sql.= "AND `pwd` = ? ";
+    $sql = "SELECT `username`, `pwd` 
+            FROM `admin` 
+            WHERE `username` = ? 
+            AND `pwd` = ? ";
 
     $arrParam = [
         $_POST['username'],
@@ -23,18 +23,17 @@ if( isset($_POST['username']) && isset($_POST['pwd']) ){
     $pdo_stmt->execute($arrParam);
 
     if( $pdo_stmt->rowCount() > 0 ){
-        //3 秒後跳頁
-        header("Refresh: 3; url=./admin.php");
-        
         //將傳送過來的 post 變數資料，放到 session，
         $_SESSION['username'] = $_POST['username'];
 
+        //3 秒後跳頁
+        header("Refresh: 3; url=./admin.php");
         echo "登入成功!!! 3秒後自動進入後端頁面";
     } else {
-        header("Refresh: 3; url=./login.php");
+        header("Refresh: 3; url=./index.php");
         echo "登入失敗…3秒後自動回登入頁";
     }
 } else {
-    header("Refresh: 3; url=./login.php");
+    header("Refresh: 3; url=./index.php");
     echo "請確實登入…3秒後自動回登入頁";
 }
